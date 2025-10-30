36858600264ef3aeada436542b2aa23471849037
robinhoodpod <minhquannaruto1@gmail.com>
2025-10-30 17:59:45 +0700

    Add files via upload

---
+++ Week 4/SelectionSort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex != i) {
+            swap(arr[i], arr[minIndex]);
+        }
+    }
+}
